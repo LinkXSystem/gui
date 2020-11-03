b270ec630268c45a742263030801298d18f1ff19
LinkXSystem <linkasde_system@163.com>
2020-11-04 00:23:29 +0800

    feat: import the think about stearm, dom to dom

--- src/core.js
+++ src/core.js
@@ -24,6 +24,7 @@ class EventBootstrap {
 class Context {
     constructor() {
         this.bootstrap = new EventBootstrap();
+        this.layers = [];
     }
 
     on(event, listener) {
@@ -32,6 +33,10 @@ class Context {
 
     emit(event, value) {
         this.bootstrap.emit(event, value);
+    }
+
+    setLayer(layer) {
+        this.layers.push(layer);
     }
 }
 
@@ -57,6 +62,8 @@ function setTool(container) {
     for (let i = 0; i < 20; i++) {
         setToolItem(tool);
     }
+
+    return tool;
 }
 
 function setToolItem(container) {
@@ -71,8 +78,71 @@ function setToolItem(container) {
     });
 }
 
+function setCanvas(container) {
+    const canvas = document.createElement('canvas');
+    canvas.style.width = '640px';
+    canvas.style.height = '480px';
+
+    canvas.width = 640;
+    canvas.height = 480;
+
+    canvas.style.boxShadow = '0px 0px 10px 0px rgb(30, 30, 30)';
+    canvas.style.position = 'fixed';
+    canvas.style.top = '50%';
+    canvas.style.left = '50%';
+    canvas.style.transform = 'translate(-50%, -50%)';
+
+    const context = canvas.getContext('2d');
+
+    context.arc(200 * Math.random(), 200 * Math.random(), 20, Math.PI / 180 * 0, Math.PI / 180 * 360, true);
+    context.stroke();
+
+    container.appendChild(canvas);
+
+    return canvas;
+}
+
+function setMonitor(container) {
+    const monitor = document.createElement('div');
+    const canvas = document.createElement('canvas');
+    monitor.appendChild(canvas);
+
+    monitor.style.position = 'fixed';
+    monitor.style.bottom = '1em';
+    monitor.style.right = '2em';
+    monitor.style.background = 'rgb(30, 30, 30)';
+
+    container.appendChild(monitor);
+
+    return monitor;
+}
+
+function setLayer(container) {
+    const layer = document.createElement('div');
+    layer.style.position = 'fixed';
+    layer.style.right = '0px';
+    layer.style.top = '50%';
+    layer.style.transform = 'translateY(-50%)';
+
+    layer.style.minWidth = '72px';
+    layer.style.minHeight = '360px';
+    layer.style.background = 'rgb(37, 37, 38)';
+
+    container.appendChild(layer);
+
+    return layer;
+}
+
 function render(container) {
     setTool(container);
+    {
+        setCanvas(container);
+        setCanvas(container);
+        setCanvas(container);
+        setCanvas(container);
+    }
+    setMonitor(container);
+    setLayer(container);
 }
 
 (function () {
